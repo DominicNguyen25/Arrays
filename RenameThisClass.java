7e52cad5b4c6b743eca22e91bc47d0976bc4e0d5
Dominic Nguyen <dominic.nguyen@mrjaffesclass.com>
2022-09-26 14:18:46 -0700

    9/26 Arrays

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,11 +9,15 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
-        for (int i = 0; i < strArray.length;i++) {
-            System.out.println(strArray[i]);
+        for (int d = 0; d < strArray.length;d++) {
+            System.out.println(strArray[d]);
         }
-        for (int i = 0; i < strArray.length;i++) {
-            System.out.print(strArray[i]);
+        for (int d = 0; d < strArray.length;d++) {
+            System.out.print(strArray[d]);
+        }
+        System.out.println();
+        for (int d = 0; d <strArray.length;d++) {
+            System.out.print("-" + strArray[d]);
         }
         }
     }
